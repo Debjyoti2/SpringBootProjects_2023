fa340d85ba735615f9dd9d2ff1e66b3432a098c5
Debjyoti <Debjyoti@Debjyoti-Kittu>
2023-05-14 11:01:20 +0530

    ok

---
+++ smartContentManager/src/main/java/com/smartcontent/SmartContentManagerApplication.java
@@ -0,0 +1,13 @@
+package com.smartcontent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartContentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartContentManagerApplication.class, args);
+	}
+
+}
